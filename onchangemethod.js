b4f02128cb9054d132b293f2960853dae0874e29
aditya894 <39375665+aditya894@users.noreply.github.com>
2024-04-04 14:35:14 +0530

    Add files via upload

---
+++ onchangemethod.js
@@ -0,0 +1,13 @@
+// onChange method implementation
+
+ const onChange = (action, data) => {
+    switch (action) {
+      case "code": {
+        setCode(data);
+        break;
+      }
+      default: {
+        console.warn("case not handled!", action, data);
+      }
+    }
+  };
